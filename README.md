9faa888198cebed7de46eaed7e2275fae4555d0c
Ofir Gal <ofir@gal.software>
2022-09-23 17:27:59 +0300

    imported the plugin from my dotfiles

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # format-on-leave.nvim
-A simple plugin to format your buffer after you leave it.
+Autocmd on `BufLeave` for synchronous `vim.lsp.buf.format` with disable/enable commands
+
+
+## Installation
+```lua
+use 'ofirgall/format-on-leave.nvim'
+```
+
+### Usage
+```lua
+-- Leave empty for default values
+require('format-on-leave').setup {
+}
+
+-- Or setup with custom parameters
+require('fomrat-on-leave').setup {
+	save_after_format = true, -- Save after the format
+	events = { 'BufLeave' },  -- When to trigger lsp format
+	pattern = { '*' },        -- Which files pattern to fomrat
+	formatting_options = nil, --Passed to `vim.lsp.buf.format` formatting_options
+	filter = nil,             -- Passed to `vim.lsp.buf.format` filter
+}
+```
+
+## Commands
+* `FormatDisable`
+* `FormatEnable`
---
+++ lua/format-on-leave/init.lua
@@ -0,0 +1,78 @@
+local M = {}
+
+local api = vim.api
+local auto_format_cmd = -1
+local loaded_config = {}
+
+M.disable_auto_format = function()
+	if auto_format_cmd ~= -1 then
+		vim.api.nvim_del_autocmd(auto_format_cmd)
+		auto_format_cmd = -1
+	end
+end
+
+M.enable_auto_format = function()
+	M.disable_auto_format()
+
+	auto_format_cmd = vim.api.nvim_create_autocmd(loaded_config.events, {
+		pattern = loaded_config.pattern,
+		callback = function(params)
+			local bufid = params.buf
+			local diff = api.nvim_get_option_value('diff', { buf = bufid })
+			if diff then
+				return
+			end
+
+			local readonly = api.nvim_get_option_value('readonly', { buf = bufid })
+			if readonly then
+				return
+			end
+
+			if #vim.lsp.get_active_clients({ bufnr = bufid }) == 0 then
+				return
+			end
+
+			-- TODO: change to async true if you can write/callback after sync,
+			-- callback = function(params)
+			-- 	for _, bufnr in ipairs(buffers_in_format) do
+			-- 		if params.buf == bufnr then
+			-- 			-- :bufdo write
+			-- 			break
+			-- 		end
+			-- 	end
+			-- end
+			vim.lsp.buf.format({
+				bufnr = bufid,
+				async = not loaded_config.save_after_format, -- Async when we dont need to save
+				formatting_options = loaded_config.formatting_options,
+				filter = loaded_config.filter
+			})
+			if loaded_config.save_after_format then
+				vim.cmd("silent! write")
+			end
+			-- table.insert(buffers_in_format, buf)
+		end
+	})
+
+end
+
+local default_config = {
+	events = { 'BufLeave' },
+	pattern = { '*' },
+	formatting_options = nil,
+	filter = nil,
+	save_after_format = true,
+}
+
+M.setup = function(config)
+	config = config or {}
+	config = vim.tbl_deep_extend('keep', config, default_config)
+
+	loaded_config = config
+
+	vim.api.nvim_create_user_command('FormatEnable', M.enable_auto_format, {})
+	vim.api.nvim_create_user_command('FormatDisable', M.disable_auto_format, {})
+	M.enable_auto_format()
+end
+
+return M
